9812a03f76d088e5f897277715f3037a256f1f83
Dina Magdy <dhammad339@gmail.com>
2025-04-08 22:06:00 +0200

    03-Department Controller Index

---
+++ MVC/Controllers/DepartmentController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC.BusinessLogic.Services;
+
+namespace MVC.Presentation.Controllers
+{
+    public class DepartmentController(IDepartmentService _departmentService) : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var departments = _departmentService.GetAllDepartments();
+            return View(departments);
+        }
+    }
+}
