e12c4b2e237abea3499ea667a8a140e78d556ecf
Dina Magdy <dhammad339@gmail.com>
2025-04-13 21:27:20 +0200

    05-Employee Module - Create Action

--- MVC/Controllers/EmployeesController.cs
+++ MVC/Controllers/EmployeesController.cs
@@ -1,15 +1,42 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MVC.BusinessLogic.DataTransferObjects.EmployeeDtos;
 using MVC.BusinessLogic.Services.Interfaces;
 using MVC.DataAccess.Repositories.Interfaces;
 
 namespace MVC.Presentation.Controllers
 {
-    public class EmployeesController (IEmployeeService _employeeService) : Controller
+    public class EmployeesController (IEmployeeService _employeeService , IWebHostEnvironment environment ,ILogger<EmployeesController> logger ) : Controller
     {
         public IActionResult Index()
         {
             var Employees = _employeeService.GetAllEmployees();
             return View(Employees);
+        }
+        [HttpGet]
+        public IActionResult Create() => View();
+        [HttpPost]
+        public IActionResult Create(CreatedEmployeeDto employeeDto)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    int Result = _employeeService.CreateEmployee(employeeDto);
+                    if (Result > 0)
+                        return RedirectToAction(nameof(Index));
+                    else
+                        ModelState.AddModelError(string.Empty, "Can’t create employee.");
+                }
+                catch(Exception ex)
+                {
+                    if(environment.IsDevelopment())
+                       ModelState.AddModelError(string.Empty, ex.Message);
+                    else
+                        logger.LogError(ex.Message);
+                }
+            }
+            return View(employeeDto);
+
         }
     }
 }
